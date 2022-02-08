8f2a3fe45e7368a36e8407010f9ef722cd2858ca
korchahin.m@dbbest.com <dantealighiery13>
2022-02-08 20:32:41 +0200

    1

--- src/components/props-to-grand-child/Child1.js
+++ src/components/props-to-grand-child/Child1.js
@@ -8,11 +8,12 @@ export const Child1 = () => {
 
   return (
     <div className="child">
-      <p>Child 1 component</p>
+      <p>Child 1 qcomponent</p>
       <p>Data is(ContexParentData): {value}</p>
       <p>Data is(ContexStaticData){data}</p>
       <ContexStaticData.Provider value={{data:'information'}}>
         <GrandChild />
+
       </ContexStaticData.Provider>
     </div>
   )
